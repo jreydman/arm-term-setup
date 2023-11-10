3834ad83d7d8e0ea508bcfde3631773da09bfbc3
Pikj [JReydman] Reyderman <58705342+jreydman@users.noreply.github.com>
2023-11-10 01:26:26 +0000

    :upd compose processes

---
+++ docs/dumps/sh-logger-manual.md
@@ -0,0 +1,19 @@
+# SH logger
+
+## Log visual
+
+log_level
+ * info
+ * warning
+ * error
+ * debug
+
+status
+ * init
+ * successful
+ * already exists
+
+execution
+```bash
+echo "Log:  <log_level> |process:element?:event?| <status>"
+```
--- embedded-static/bash/compose.sh
+++ embedded-static/bash/compose.sh
@@ -1,7 +1,21 @@
-# run func prompt for upload [homebrew]
-brew-install() {
-echo "Log:	<info>	|homebrew installation|	init"
-command -v brew &> /dev/null && echo "Log:	<warning>	|homebrew installation| already exists" || /bin/bash -i -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &
+homebrew-install() {
+echo "Log:	<info>	|homebrew:install|	init"
+command -v brew &> /dev/null && echo "Log:	<warning>	|homebrew:install| already exists" || /bin/bash -i -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &
 wait
-echo "Log:	<info>	|homebrew installation| successful"
+echo "Log:	<info>	|homebrew:install| successful"
+}
+
+zsh-paths-refresh() {
+echo "Log: <info>	|zsh:paths:check|	init"
+[ -d $ZSH ] || mkdir -p $ZSH && echo "Log:	<warning>	|zsh:paths:check|>flow[DATA]	already exists"
+[ -d $ZSH_CONFIG ] || mkdir -p $ZSH_CONFIG && echo "Log:	<warning>	|zsh:paths:check|>flow[CONFIG]	already exists"
+[ -d $ZSH_CACHE ] || mkdir -p $ZSH_CACHE && echo "Log:	<warning>	|zsh:paths:check|>flow[CACHE]	already exists"
+[ -d $ZSH_TEMP ] || mkdir -p $ZSH_TEMP && echo "Log:	<warning>	|zsh:paths:check|>flow[TEMP]	already exists"
+echo "Log: <info>	|zsh:paths:check|	successful"
+}
+
+zsh-ln-refresh() {
+echo "Log: <info>	|zsh:ln:check|	init"
+[ -e $HOME/.zprofile ] || ln -s $ZSH_CONFIG/.zprofile $HOME/.zprofile && echo "Log: <warning>	|zsh:ln:check|>flow[zprofile]	already exists"
+echo "Log: <info>	|zsh:ln:check|	successful"
 }
