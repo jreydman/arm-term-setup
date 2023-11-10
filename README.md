539f7356f791b42986cac8b0ff28c40b2a6b8840
Pikj [JReydman] Reyderman <58705342+jreydman@users.noreply.github.com>
2023-11-10 10:46:29 +0000

    :upd compose

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 codename: ascendI
 
+specification: XDG
+
+Logic:
+
+ascendI.exe > bash[init] {
+    bash:[compose]>SETUP
+    1. checkin rcfile environments > xdg:config:[shell]:rcfile
+       1. ln xdg:config:[shell]:rcfile > ~/rcfile
+    2. checkin [aum]
+       1. [aum]:refresh/update
+       2. [aum]:upload ext > ~/.rcfile
+    3. bash[compose]:reboot
+} > 
+
 ---
 
 ## Relation links
--- embedded-static/bash/compose.sh
+++ embedded-static/bash/compose.sh
@@ -5,12 +5,18 @@
 # XDG_CACHE_HOME=$HOME/Library/Caches
 # XDG_RUNTIME_DIR=$HOME/Library/Caches/TemporaryItems
 
+RC_PROFILE_FILE=.zprofile
+RC_FILE=.zshrc
+
 #########################################################
+
+logger() { echo "Log: <$1>    |$2|    $3"; }
+logger:flow() { echo "Log: <$1>    |$2|>flow[$3]    $4"; }
 
 xdg-environment-refresh() {
   check_and_set() {
     local var_name=$1; local var_path=$2
-    [ -z "${!var_name}" ] && eval "$var_name=\$HOME/$var_path" && echo "Log:	<info>	|xdg:environment:check|>flow[$var_path] successful" || echo "Log:	<warning>	|xdg:environment:check|>flow[$var_path] already exists"
+    [ -z "${!var_name}" ] && eval "$var_name=\$HOME/$var_path" && logger:flow info> xdg:environment:check $var_path successful || logger:flow warning xdg:environment:check $var_path "already exists"
   }
   check_and_set XDG_DATA_HOME .local/share
   check_and_set XDG_CONFIG_HOME .config
@@ -19,39 +25,41 @@ xdg-environment-refresh() {
 }
 
 homebrew-install() {
-echo "Log:	<info>	|homebrew:install|	init"
-command -v brew &> /dev/null && { echo "Log:	<warning>	|homebrew:install| already exists"; return; }
+logger info homebrew:install init
+command -v brew &> /dev/null && { logger warning> homebrew:install "already exists"; return; }
 /bin/bash -i -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
 wait &&
-echo "Log:	<info>	|homebrew:install|>flow[opt-write]	successful" &&
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zshrc &&
+logger:flow info homebrew:install opt-write	successful &&
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/$RC_FILE &&
 eval "$(/opt/homebrew/bin/brew shellenv)" &&
-echo "Log:	<info>	|homebrew:install|>flow[init] successful"
+logger:flow info homebrew:install init successful
 }
 
 zsh-paths-refresh() {
-echo "Log: <info>	|zsh:paths:check|	init"
+logger info zsh:paths:check init
 directories=("$ZSH" "$ZSH_CONFIG" "$ZSH_CACHE" "$ZSH_TEMP")
 for dir in "${directories[@]}"; do
-    [ -d "$dir" ] || mkdir -p "$dir" && echo "Log:	<warning>	|zsh:paths:check|>flow[${dir}]	already exists"
+    [ -d "$dir" ] || mkdir -p "$dir" && logger:flow warning zsh:paths:check ${dir} "already exists"
 done
-echo "Log: <info>	|zsh:paths:check|	successful"
+logger info zsh:paths:check	successful
 }
 
 zsh-ln-refresh() {
-echo "Log: <info>	|zsh:ln:check|	init"
-[ -e $HOME/.zprofile ] || ln -s $(eval echo "$ZSH_CONFIG/.zprofile") $HOME/.zprofile && echo "Log: <warning>	|zsh:ln:check|>flow[zprofile]	already exists"
-[ -e $HOME/.zshrc ] || ln -s $(eval echo "$ZSH_CONFIG/.zshrc") $HOME/.zshrc && echo "Log: <warning>	|zsh:ln:check|>flow[zshrc]	already exists"
-echo "Log: <info>	|zsh:ln:check|	successful"
+logger info zsh:ln:check init
+links=("$RC_FILE" "$RC_PROFILE_FILE")
+for link in "${links[@]}"; do
+[ ! -e "$HOME/$link" ] && ln -s "$(eval echo "$ZSH_CONFIG/$link")" "$HOME/$link" && logger:flow info zsh:ln:check $link created || logger:flow warning zsh:ln:check $link "already exists"
+done
+logger info zsh:ln:check successful
 }
 
 zsh-dump-rc() {
 xdg-environment-refresh
-echo "Log: <info>	|zsh:dump:rc|	init"
+logger info zsh:dump:rc init
 ZSH_CONFIG=$XDG_CONFIG_HOME/zsh
 rm -rf $(eval echo "$ZSH_CONFIG")
 mkdir -p $(eval echo "$ZSH_CONFIG")
-cat > $(eval echo "$ZSH_CONFIG/.zshrc") <<EOL
+cat > $(eval echo "$ZSH_CONFIG/$RC_FILE") <<EOL
 #########################################################
 # 		OVERWRITE -- [protected]		#
 #-------------------------------------------------------#
@@ -84,23 +92,25 @@ export HOMEBREW_PREFIX=\$XDG_CONFIG_HOME/homebrew
 export HOMEBREW_CACHE=\$XDG_CACHE_HOME/homebrew
 export HOMEBREW_TEMP=\$XDG_RUNTIME_DIR/homebrew
 #########################################################
-# -- EXTENSIONS
+# -- EXTENSIONS -- [protected]
 EOL
-echo "Log: <info>	|zsh:dump:rc|>flow[z-write]	successful"
-source $(eval echo "$ZSH_CONFIG/.zshrc") && echo "Log:	<info>	|root:source:check|>flow[zshrc] successful"
+logger:flow info zsh:dump:rc z-write successful
+source $(eval echo "$ZSH_CONFIG/$RC_FILE") && logger:flow info root:source:check $RC_FILE successful
 zsh-ln-refresh
 
 #[homebrew] ext #########################################
-command -v brew &> /dev/null && { (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zshrc && wait; }
+command -v brew &> /dev/null && { (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/$RC_FILE && wait; }
 }
 
+# -- OVERRIDE -- [protected] > rcprofile
+# [ -e $(eval echo "$ZSH_CONFIG/$RC_PROFILE_FILE") ] || echo "Hello, this is the content of the file!" > $HOME/$RC_PROFILE_FILE
 #########################################################
-
 SETUP() {
 zsh-dump-rc		# initialise zsh
 zsh-paths-refresh	# initialise xdg infrastructure
 homebrew-install	# checkin homebrew
 }
-
 #########################################################
-SETUP
+# SETUP
+zsh-ln-refresh
+logger:flow info bash:compose:log close successful
