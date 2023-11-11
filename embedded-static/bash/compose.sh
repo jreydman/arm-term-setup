6f8f209790431d47bd84dfd126cead3b76650ec3
Pikj Reyderman <jreydman@MacBook-Air-Pikj.local>
2023-11-11 17:53:40 +0200

    :refactor env&paths

--- embedded-static/bash/compose.sh
+++ embedded-static/bash/compose.sh
@@ -13,6 +13,9 @@ SH_SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 # XDG_RUNTIME_DIR=$HOME/Library/Caches/TemporaryItems	#
 #-------------------------------------------------------#
 
+XDG_ENVIRONMENTS=("XDG_DATA_HOME" "XDG_CONFIG_HOME" "XDG_CACHE_HOME" "XDG_RUNTIME_DIR")
+XDG_DEFAULT_VALUES=("$HOME/.local/share" "$HOME/.config" "$HOME/.cache" "$HOME/.tmp")
+
 RC_PROFILE_FILE=.zprofile
 RC_FILE=.zshrc
 RC_ENV_FILE=.zshenv
@@ -21,16 +24,34 @@ RC_ENV_FILE=.zshenv
 logger() { printf "($SH_SCRIPT_DIR)|Log: <%s>\t|%s|\t%s\n" "$1" "$2" "$3"; }
 logger:flow() { printf "($SH_SCRIPT_DIR)|Log: <%s>\t|%s|>flow[%s]\t%s\n" "$1" "$2" "$3" "$4"; }
 
-xdg-environment-refresh() {
-  check_and_set() {
-    local var_name=$1; local var_path=$2
-    [ -z "${!var_name}" ] && eval "$var_name=\$HOME/$var_path" && logger:flow info xdg:environment:check $var_path successful || logger:flow warning xdg:environment:check $var_path "already exists"
-  }
-  check_and_set XDG_DATA_HOME .local/share
-  check_and_set XDG_CONFIG_HOME .config
-  check_and_set XDG_CACHE_HOME .cache
-  check_and_set XDG_RUNTIME_DIR .tmp
+environments_check() { # $1[env_names] $[env_default_values]
+  env_vars=("${!1}")
+  default_values=("${!2}")
+  for ((i=0; i<${#env_vars[@]}; i++)); do
+    env_var="${env_vars[$i]}"
+    default_value="${default_values[$i]}"
+    if eval "[ -z \${$env_var+x} ]"; then
+      eval "$env_var='$default_value'"
+      logger:flow info "env:check:$env_var" ${!env_var} "successful"
+    else logger:flow warning env:check:$env_var ${!env_var} "already exists"
+    fi
+  done
 }
+
+paths_check() { # $1[env_name_paths] $2"force"?
+echo $2
+logger info "paths:check" init
+env_vars=("${!1}")
+for env_name_path in "${env_vars[@]}"; do
+path=$(eval echo "\${$env_name_path}")
+if [ -d "$path" ] && [ -z "$2" ]; then logger:flow warning "paths:check:$env_name_path" ${path} "already exists";
+elif [ -d "$path" ] && [ ! -z "$2" ]; then rm -rf "$path"; mkdir -p "$path"; logger:flow info "paths:check<force>:$env_name_path" ${path} successful;
+else mkdir -p "$path"; logger:flow info "paths:check:$env_name_path" ${path} successful;
+fi
+done
+logger info "paths:check" successful
+}
+
 homebrew-uninstall() { logger info homebrew:uninstall init; rm -rf $(eval echo $HOMEBREW_PREFIX); rm -rf $(eval echo $HOMEBREW_REPOSITYRY); rm -rf $(eval echo $HOMEBREW_CACHE); rm -rf $(eval echo $HOMEBREW_TEMP); export PATH=$(echo $PATH | tr ':' '\n' | grep -v $(eval echo $HOMEBREW_PREFIX)/bin | tr '\n' ':'); export PATH=$(echo $PATH | tr ':' '\n' | grep -v $(eval echo $HOMEBREW_PREFIX)/sbin | tr '\n' ':'); logger info homebrew:uninstall successful; }
 homebrew-check() {
 logger info homebrew:check init
@@ -38,15 +59,6 @@ PATH="/opt/homebrew/bin:$PATH"
 command -v brew &> /dev/null && { logger:flow warning homebrew:check install "already exists"; return; }
 mkdir $(eval echo $HOMEBREW_PREFIX) && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $(eval echo $HOMEBREW_PREFIX) &&
 wait && logger:flow info homebrew:check init successful
-}
-
-zsh-paths-refresh() {
-logger info zsh:paths:check init
-directories=("$ZSH" "$ZSH_CONFIG" "$ZSH_CACHE" "$ZSH_TEMP")
-for dir in "${directories[@]}"; do
-    [ -d "$dir" ] || mkdir -p "$dir" && logger:flow warning zsh:paths:check ${dir} "already exists"
-done
-logger info zsh:paths:check	successful
 }
 
 zsh-rc-check() { source $(eval echo "$ZSH_CONFIG/$RC_FILE") && logger:flow info root:source:check $RC_FILE successful; }
@@ -61,12 +73,19 @@ logger info zsh:ln:check successful
 }
 
 zsh-rc-dump() {
-xdg-environment-refresh
 logger info zsh:rc:dump init
 ZSH_CONFIG=$XDG_CONFIG_HOME/zsh
-find $(eval echo "$ZSH_CONFIG")/ -mindepth 1 -maxdepth 1 -not -name $RC_PROFILE_FILE -exec rm -r {} +
-
-[ ! -e $(eval echo "$ZSH_CONFIG/$RC_PROFILE_FILE") ] && touch $(eval echo "$ZSH_CONFIG/$RC_PROFILE_FILE") && logger:flow info zsh:rc:dump $RC_PROFILE_FILE created || logger:flow warning zsh:rc:dump $RC_PROFILE_FILE "already exists"
+echo $ZSH_CONFIG
+# Проверяем существование папки и файла
+if [ ! -d "$ZSH_CONFIG" ] || [ ! -e "$RC_PROFILE_PATH" ]; then
+    # Если что-то не существует, создаем папку и файл
+    mkdir -p "$ZSH_CONFIG"
+    touch "$RC_PROFILE_PATH"
+    logger:flow info zsh:rc:dump $RC_PROFILE_FILE created
+else
+    # Если оба существуют, выводим логгер "already exists"
+    logger:flow warning zsh:rc:dump $RC_PROFILE_FILE "already exists"
+fi
 
 cat > $(eval echo "$ZSH_CONFIG/$RC_FILE") <<EOL
 #########################################################
@@ -128,15 +147,16 @@ logger info zsh:rc:dump successful
 # [ -e $(eval echo "$ZSH_CONFIG/$RC_PROFILE_FILE") ] || echo "Hello, this is the content of the file!" > $HOME/$RC_PROFILE_FILE
 #########################################################
 SETUP() {
-zsh-paths-refresh	# initialise xdg infrastructure
-zsh-rc-dump		# initialise zsh
-homebrew-check		# checkin homebrew
+environments_check "XDG_ENVIRONMENTS[@]" "XDG_DEFAULT_VALUES[@]"
+paths_check "XDG_ENVIRONMENTS[@]" # -f
+#zsh-paths-refresh	# initialise xdg infrastructure
+#zsh-rc-dump		# initialise zsh
+#homebrew-check		# checkin homebrew
 }
 #########################################################
 SETUP
 # homebrew-uninstall
 logger:flow info bash:compose:log close successful
-
 # IN LAST
-zsh-rc-check
-clear && printf '\e[3J' && cat $HOME/$RC_FILE
+# zsh-rc-check
+# clear && printf '\e[3J'
